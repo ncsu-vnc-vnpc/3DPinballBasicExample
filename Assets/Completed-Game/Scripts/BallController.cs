ac36c5760623988ca60081e58735b6e9519775d5
Todd Berreth <tmberret@ncsu.edu>
2020-01-23 14:54:26 -0500

    new files

---
+++ Assets/Completed-Game/Scripts/BallController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+public class BallController : MonoBehaviour
+{
+
+    public float speed;
+    // Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+    private Rigidbody rb;
+
+
+    // At the start of the game..
+    void Start()
+    {
+        // Assign the Rigidbody component to our private rb variable
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Each physics step..
+    void FixedUpdate()
+    {
+
+        /*
+        // Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
+        // multiplying it by 'speed' - our public player speed that appears in the inspector
+        rb.AddForce(movement * speed);
+        */
+    }
+}
